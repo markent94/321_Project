2dd4bea3df9b952f46afa6d53033b6dc76c951cf
markent94 <mrb600@hawaii.edu>
2014-04-24 14:26:19 -1000

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,28 @@ public class test{
    public static void main(String[] args) {
       System.out.println("test. TEST!");
       
+package ics321proj;
+
+public class Items {
+	
+	private String name;
+	private int attack;
+	private int health;
+	private int defense;
+	private int block;
+	private int crit;
+	
+	public Items() {
+		name = null;
+		attack = 0;
+		health = 0;
+		defense = 0;
+		block = 0;
+		crit = 0;	
+	}
+}
+
+      
    }
 }
       
